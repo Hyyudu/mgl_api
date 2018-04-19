844b1500571516c228a974f66fecc781e8fe41d5
Hyyudu <hyyudu@gmail.com>
2018-04-19 15:12:30 +0300

    error handling

--- src/handlers/GetCodesHandler.py
+++ src/handlers/GetCodesHandler.py
@@ -1,6 +1,6 @@
 import json
 
-from tornado.httpclient import AsyncHTTPClient
+from tornado.httpclient import AsyncHTTPClient, HTTPClient, HTTPError
 from tornado.web import RequestHandler
 
 
@@ -10,12 +10,17 @@ class GetCodeAsyncHandler(RequestHandler):
         http_client = AsyncHTTPClient()
         urls_to_check = json.loads(self.request.body)
         for url in urls_to_check:
-            response = await http_client.fetch(url)
-            res[url] = response.code
+            try:
+                response = await http_client.fetch(url)
+                res[url] = response.code
+            except HTTPError as e:
+                res[url] = e.code
+            except Exception as e:
+                res[url] = str(e)
         self.write(json.dumps(res))
 
 # class GetCodeHandler(RequestHandler):
-#     def get(self):
+#     def post(self):
 #         http_client = HTTPClient()
 #         try:
 #             response = http_client.fetch("http://www.google.com/")
