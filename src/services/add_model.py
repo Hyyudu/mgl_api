54389e901a081d8b5fcb667f5fe46ef1c70dccf2
Hyyudu <hyyudu@gmail.com>
2018-05-24 07:45:01 +0300

    DB insert many

--- src/services/add_model.py
+++ src/services/add_model.py
@@ -5,3 +5,12 @@ db = DB()
 def add_model(data):
     new_model_id = db.insert('models', data)
     model_params = db.fetchColumn('select parameter_code from model_has_parameters where node_code=:node_type_code', data)
+    insert_parameters = [
+        {
+            "model_id": new_model_id,
+            "parameter_code": code,
+            "value": data['params'].get(code, 0)
+        }
+        for code in model_params
+    ]
+    db.insert('model_parameters', insert_parameters)
